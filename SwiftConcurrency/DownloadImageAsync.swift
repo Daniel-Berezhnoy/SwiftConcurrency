93b2d50ae6dfaca52f099c0c1d7005e60e7a7a18
Daniel Berezhnoy <43868541+Daniil-Berezhnyi@users.noreply.github.com>
2022-12-20 17:08:22 -0800

    Lil refactor

--- SwiftConcurrency/DownloadImageAsync.swift
+++ SwiftConcurrency/DownloadImageAsync.swift
@@ -12,6 +12,13 @@ struct DownloadImageAsync: View {
     
     var body: some View {
         VStack {
+            image
+            button
+        }
+    }
+    
+    var image: some View {
+        ZStack {
             if let image = viewModel.image {
                 image
                     .resizable()
@@ -20,11 +27,22 @@ struct DownloadImageAsync: View {
                     .frame(width: 250)
                     .cornerRadius(20)
             }
-            
-            Button("Load Image") { viewModel.getImage() }
-                .buttonStyle(.borderedProminent)
-                .padding(30)
         }
+    }
+    
+    var button: some View {
+        Button {
+            Task {
+                await viewModel.getImage()
+            }
+        } label: {
+            Label("Load Image", systemImage: "photo")
+                .padding(5)
+                .font(.headline)
+        }
+        .buttonStyle(.bordered)
+        .tint(.orange)
+        .padding(.top, 30)
     }
 }
 
@@ -36,30 +54,26 @@ struct DownloadImageAsync_Previews: PreviewProvider {
 
 class DownloadImageAsyncViewModel: ObservableObject {
     
-    @Published var image = Image(systemName: "swift")
+    @Published var image: Image? = Image(systemName: "swift")
     let loader = ImageLoader()
     
-    func getImage() {
-        loader.downloadWithEscaping { [weak self] image, error in
-            DispatchQueue.main.async {
-                if let image {
-                    self?.image = Image(uiImage: image)
-                }
-            }
-        }
+    func getImage() async {
+//        loader.downloadWithEscaping { [weak self] image, error in
+//            DispatchQueue.main.async {
+//                if let image {
+//                    self?.image = Image(uiImage: image)
+//                }
+//            }
+//        }
+        
+        let image = try? await loader.downloadWithAsync()
+        self.image = Image(uiImage: image ?? UIImage(systemName: "swift")!)
     }
 }
 
 class ImageLoader {
-    let url = URL(string: "https://picsum.photos/200")!
     
-    func downloadWithEscaping(cHandler: @escaping (_ image: UIImage?, _ error: Error?) -> Void) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            let image = self.handleResponse(data: data, response: response)
-            cHandler(image, error)
-        }
-        .resume()
-    }
+    let url = URL(string: "https://picsum.photos/200")!
     
     func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
         guard
@@ -72,4 +86,21 @@ class ImageLoader {
         
         return image
     }
+    
+    func downloadWithEscaping(handler: @escaping (_ image: UIImage?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            let image = self?.handleResponse(data: data, response: response)
+            handler(image, error)
+        }
+        .resume()
+    }
+    
+    func downloadWithAsync() async throws -> UIImage? {
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            return handleResponse(data: data, response: response)
+        } catch {
+            throw error
+        }
+    } 
 }
